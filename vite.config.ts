6d9b0695c99a197977f67b90a7df195d088f1639
AkshayAD <akshaydewalwar305@gmail.com>
2026-01-10 16:30:24 +0530

    Change base path for production environmentFix base path for GitHub Pages after repo rename

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   const isProd = mode === 'production';
   return {
-    base: isProd ? '/Cook-Commander/' : '/',
+    base: isProd ? '/QookCommander/' : '/',
     server: {
       port: 3000,
       host: '0.0.0.0',
